f223374c8ff8a001e19920e1cf5353b2e9e6749a
Alastair Parker <alastairparker@192-168-1-4.tpgi.com.au>
2018-10-24 15:27:08 +0800

    homepage component set out

--- App.js
+++ App.js
@@ -1,13 +1,7 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+
+import Home from './src/components/home/home';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -18,48 +12,10 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
-
-  state = {
-    placeName: '',
-    university: '',
-  }
-
-  placeNameChangeHandler = val => {
-    this.setState({
-      placeName: val
-    });
-  }
-
-  universityChangeHandler = val => {
-    this.setState({
-      university: val
-    });
-  }
-
-  bah = () => {
-    alert(this.state.placeName);
-  }
-
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to Drivee!</Text>
-        <Text style={styles.instructions}>Rewarding safe driving habits</Text>
-        <Text style={styles.questionHeading}>What's your name?</Text>
-        <View style={styles.inputContainer}>
-          <TextInput 
-            style={styles.nameInput}
-            placeholder="Alastair Parker"
-            value={this.state.placeName}
-            onChangeText={this.placeNameChangeHandler}
-          />
-
-          <TouchableOpacity 
-            onPress={this.bah}
-          >
-            <Text style={styles.nameButton}> Submit </Text>
-          </TouchableOpacity>
-        </View>
+        <Home />
       </View>
     );
   }
@@ -72,42 +28,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  inputContainer: {
-    width: "100%",
-    flexDirection: "row",
-    justifyContent: "space-around",
-    alignItems: "center",
-    marginTop: 10,
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-  nameInput: {
-    width: "70%",
-    borderColor: 'black',
-    borderWidth: 1,
-    padding: 10,
-  },
-  nameInput: {
-    width: "70%",
-    borderColor: 'black',
-    borderWidth: 1,
-    padding: 10,
-  },
-  nameButton: {
-    width: "100%",
-  },
-  questionHeading: {
-    marginTop: 50,
-  },
-  questionHeading2: {
-    marginTop: 20,
-  }
+  
 });
---
+++ src/components/home/home.js
@@ -0,0 +1,90 @@
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+
+class Home extends Component {
+	state = {
+		name: '',
+	}
+	
+	nameChangeHandler = val => {
+		this.setState({
+			name: val
+		});
+	}
+  render() {
+    return (
+      <View style={styles.container}>
+				<Text style={styles.welcome}>Welcome to Drivee!</Text>
+				<Text style={styles.instructions}>Rewarding safe driving habits</Text>
+				<Text style={styles.questionHeading}>What's your name?</Text>
+				
+				<View style={styles.inputContainer}>
+					<TextInput 
+						style={styles.nameInput}
+						placeholder="Alastair Parker"
+						value={this.state.name}
+						onChangeText={this.nameChangeHandler}
+					/>
+
+					<TouchableOpacity 
+						onPress={this.nameSubmitHandler}
+					>
+						<Text style={styles.nameButton}> Submit </Text>
+					</TouchableOpacity>
+				</View>
+
+				<View>
+					<Text>Your Name is: {this.state.name}</Text>
+				</View>
+			</View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#F3D452',
+		padding: 10,
+	},
+	inputContainer: {
+		width: "100%",
+		flexDirection: "row",
+		justifyContent: "space-around",
+		alignItems: "center",
+		marginTop: 10,
+	},
+	welcome: {
+		fontSize: 20,
+		textAlign: 'center',
+		margin: 10,
+	},
+	instructions: {
+		textAlign: 'center',
+		color: '#333333',
+		marginBottom: 5,
+	},
+	nameInput: {
+		width: "70%",
+		borderColor: 'black',
+		borderWidth: 1,
+		borderRadius: 5,
+		padding: 10,
+	},
+	nameButton: {
+		borderWidth: 1,
+		borderColor: '#0D4042',
+		borderRadius: 3,
+		padding: 3,
+	},
+	questionHeading: {
+		marginTop: 50,
+	},
+	questionHeading2: {
+		marginTop: 20,
+	}
+});
+
+export default Home;
